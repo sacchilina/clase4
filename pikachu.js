f9612931105ffc27844e08e1868f4da579fcc6cd
sacchilina <sacchilina@gmail.com>
2022-03-13 16:21:06 -0300

    cambiando formato

--- pikachu.js
+++ pikachu.js
@@ -6,4 +6,10 @@ Pikachu
 
 Yo quiero ser siempre el mejor, mejor que nadie mas 
 
+No pero a mi no me gusto tu modificacion quiero que sea asi:
+atraparlos mi prueba es
+entrenarlos mi ideal
+pokemon!
+tengo que atraparlos
+
 
