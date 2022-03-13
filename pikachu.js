bf980f855b88f993e3788e577e095160b861c7ca
sacchilina <sacchilina@gmail.com>
2022-03-13 16:20:16 -0300

    aceptando modificacion local correctamente

--- pikachu.js
+++ pikachu.js
@@ -4,14 +4,6 @@ Pika pikaaaaaaaa
 
 Pikachu
 
-<<<<<<< HEAD
 Yo quiero ser siempre el mejor, mejor que nadie mas 
 
 
-=======
-Yo quiero ser siempre el mejor
-mejor que nadie mas
-atraparlos mi prueba es 
-entrenarlos mi ideal
-pokemon!
->>>>>>> 807b8d21ae0662235353267e4275014ef954e616
