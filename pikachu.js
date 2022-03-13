025f464ac6198ff88cd8030463ed8dafbd2b0e41
sacchilina <sacchilina@gmail.com>
2022-03-13 16:26:59 -0300

    conflicto resuelto

--- pikachu.js
+++ pikachu.js
@@ -4,12 +4,8 @@ Pika pikaaaaaaaa
 
 Pikachu
 
-Yo quiero ser siempre el mejor, mejor que nadie mas 
-
-No pero a mi no me gusto tu modificacion quiero que sea asi:
-atraparlos mi prueba es
+Yo quiero ser siempre el mejor
+mejor que nadie mas
+atraparlos mi prueba es 
 entrenarlos mi ideal
-pokemon!
-tengo que atraparlos
-
 
